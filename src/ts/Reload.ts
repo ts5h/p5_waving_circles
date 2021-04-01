246c2a301b19e70342c1580dd57098317fc153f7
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-04-01 20:46:20 +0200

    Update the reload button

--- src/ts/Reload.ts
+++ src/ts/Reload.ts
@@ -1,12 +1,24 @@
+import isMobile from 'ismobilejs';
+
 // Reload
 const reloadButton = document.querySelector('.reload');
 
-reloadButton.addEventListener('mouseover', function () {
-  this.classList.add('on');
-});
-reloadButton.addEventListener('mouseout', function () {
-  this.classList.remove('on');
-});
 reloadButton.addEventListener('click', function () {
   location.reload();
 });
+
+if (isMobile().tablet || isMobile().phone) {
+  reloadButton.addEventListener('touchstart', function () {
+    this.classList.add('on');
+  });
+  reloadButton.addEventListener('touchend', function () {
+    this.classList.remove('on');
+  });
+} else {
+  reloadButton.addEventListener('mouseover', function () {
+    this.classList.add('on');
+  });
+  reloadButton.addEventListener('mouseout', function () {
+    this.classList.remove('on');
+  });
+}
