3f16f49026bbe4adaa163f20d1456ac13003a850
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-04-01 17:02:38 +0200

    Update drawing functions

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Circles
-Circles using p5.js+Webpack+Dart Sass (SCSS)
+Circles using p5.js + TypeScript + Webpack + Dart Sass (SCSS)
 
 
 References:
-- https://www.webdelog.info/entry/2016/06/12/arranged-in-the-circumferential-direction.html
-- https://note.com/deconbatch/n/nc14219bfacc6
+- 円に沿ってオブジェクトを配置する - Webdelog
+https://www.webdelog.info/entry/2016/06/12/arranged-in-the-circumferential-direction.html
+https://codepen.io/turusuke/pen/c787cd679140940b3af29977c7794b8c
+  
+- 周期的パーリンノイズ - note / deconbatch
+https://note.com/deconbatch/n/nc14219bfacc6
+  
+- p5.jsで心のままにcurveVertex()探索 - 01
+https://infosmith.biz/blog/it/p5js-curvevertex-2
--- src/ts/circle.ts
+++ src/ts/circle.ts
@@ -1,61 +1,109 @@
-import * as p5 from 'p5';
+import * as P5 from 'p5';
 
-const sketch = (p: p5) => {
-  const noiseScale = .001;
-  const setting = {
-    circleNum: 400,
-    cx: 0,
-    cy: 0,
-    r: 0,
+interface circle {
+  pointNum: number,
+  cx: number,
+  cy: number,
+  r: number,
+  degree: number,
+  noiseScale: number,
+  opacity: number,
+  seed: number,
+  seedStep: number
+}
+
+const sketch = (p5: P5) => {
+  const circles = Array(100);
+
+  const setCoordination = (c: circle) => {
+    c.cx = p5.width / 2;
+    c.cy = p5.height / 2;
+    c.r = p5.min(p5.windowWidth, p5.windowHeight) / 2;
   };
-  let r = 0;
-  let seed = [10];
 
-  p.preload = () => {
+  p5.preload = () => {
     //
   };
 
-  p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    setting.cx = p.width / 2;
-    setting.cy = p.height / 2;
-    setting.r = Math.min(p.windowWidth, p.windowHeight) / 2;
-    r = setting.r;
+  p5.setup = () => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
 
-    // p.noLoop();
+    [...new Array(circles.length)].forEach((_, i) => {
+      circles[i] = {};
+      setCoordination(circles[i]);
+
+      circles[i].pointNum = p5.floor(p5.random(20, 50));
+      circles[i].degree = p5.random(-90, 90);
+      circles[i].noiseScale = .01;
+      circles[i].seed = p5.random(100);
+      circles[i].seedStep = p5.random(.001, .0025);
+    });
+
+    p5.strokeWeight(.1);
+    // p5.noLoop();
+
+    p5.background('#333')
   };
 
-  p.draw = () => {
-    p.clear();
-    p.background('#333');
+  p5.draw = () => {
+    // p5.clear();
+    p5.background('#333');
 
     // Circular Dots
-    p.smooth();
-    p.stroke(255);
-    p.noFill();
+    p5.smooth();
+    p5.stroke(255);
+    p5.noFill();
 
     // Circular Dots
-    [...new Array(20)].forEach((_, i) => {
-      [...new Array(setting.circleNum)].forEach((_, j) => {
-        const rad = j / setting.circleNum * p.PI * 2;
-        const pN = p.noise(
-          r * p.cos(rad) * noiseScale + setting.cx,
-          r * p.sin(rad) * noiseScale + setting.cy,
-          seed[i]
+    circles.forEach((c, i) => {
+      const startPoints = [];
+      p5.beginShape();
+
+      [...new Array(c.pointNum)].forEach((_, j) => {
+        const rad = j / c.pointNum * p5.PI * 2 + c.degree;
+
+        const pN = p5.noise(
+            c.r * p5.cos(rad) * c.noiseScale + c.cx,
+            c.r * p5.sin(rad) * c.noiseScale + c.cy,
+            c.seed
         );
-        const pR = r * p.noise(pN);
-        const x = pR * p.cos(rad) + setting.cx;
-        const y = pR * p.sin(rad) + setting.cy;
-        p.point(x, y);
+        const pR = c.r * p5.noise(pN);
+
+        const x = pR * p5.cos(rad) + c.cx;
+        const y = pR * p5.sin(rad) + c.cy;
+
+        // Draw points for debug
+        // p5.stroke('red');
+        // p5.point(x, y);
+
+        // Draw curve vertex
+        // p5.stroke(255);
+        p5.curveVertex(x, y);
+
+        // Set 3 points for closing
+        if (j < 3) {
+          startPoints[j] = {};
+          startPoints[j].x = x;
+          startPoints[j].y = y;
+        }
       });
 
-      seed[i] += .001;
+      // Draw residual curve vertex
+      startPoints.forEach((c, j) => {
+        p5.curveVertex(c.x, c.y);
+      });
+
+      p5.endShape();
+      c.seed += c.seedStep;
     });
   };
 
-  p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  p5.windowResized = () => {
+    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    circles.forEach((c, _) => {
+      setCoordination(c);
+    });
   }
 };
 
-new p5(sketch);
+new P5(sketch);
